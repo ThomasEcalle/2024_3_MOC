6cccbf6c515ad6f39e450a0244615f0a87eeefde
ThomasEcalle <thomasecalle@gmail.com>
2024-04-17 16:15:28 +0200

    Add Scaffold part with app bar, bottom bar & floating button

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,13 +6,41 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-        child: Text(
-          'Coucou',
-          style: Theme.of(context).textTheme.displayLarge,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('coucou'),
+        leading: const Icon(Icons.airplane_ticket),
+        actions: const [
+          Icon(Icons.search),
+          Icon(Icons.settings),
+        ],
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Center(
+          child: Text(
+            'Coucou',
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Coucou');
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'settings',
+          ),
+        ],
       ),
     );
   }

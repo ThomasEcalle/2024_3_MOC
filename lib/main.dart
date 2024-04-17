5c7172e597f5cd8a8c70c995d770e3864499f587
ThomasEcalle <thomasecalle@gmail.com>
2024-04-17 15:08:29 +0200

    Add centered text with style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:moc_2024_3/home.dart';
 
 void main() {
-  runApp(Container(color: Colors.orange));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 30,
+            color: Colors.orange,
+          ),
+          displayMedium: TextStyle(
+            fontSize: 25,
+            color: Colors.orange,
+          ),
+        ),
+      ),
+      home: const Home(),
+    );
+  }
 }
